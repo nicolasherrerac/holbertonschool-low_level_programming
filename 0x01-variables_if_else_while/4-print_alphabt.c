8c4d0a7a39f6b483125c87d81e9c193a6e75fe56
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-17 21:57:09 +0000

    shows all the alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main -show all the alphabet except q and e
+* Description: show all the alphabet except q and e
+* Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	if (ch != 'q')
+	if (ch != 'q')
+
+	putchar('\n');
+}
