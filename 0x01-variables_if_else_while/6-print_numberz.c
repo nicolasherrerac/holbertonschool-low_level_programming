c5a8812ce67e5598eb6245a7f82167fbb12d36a6
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-17 23:30:29 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Numberz
+* Description:prints numbers of base 10 starting from 0 followed by a new line.
+* Return: 0
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
