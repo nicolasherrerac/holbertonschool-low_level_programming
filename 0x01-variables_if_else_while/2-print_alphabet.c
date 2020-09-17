5b494b59c0b92a570495fe50be5388aef1c6d457
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-17 20:13:47 +0000

    Display all alphabetic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Alphabetic
+* Description: Display all alphabetic
+* Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
