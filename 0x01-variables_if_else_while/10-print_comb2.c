86f5586e4df59e2f30e42cb84ad53aad80667a55
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-18 02:23:34 +0000

    00..99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - 00...99
+* Description: Write a program that prints the numbers from 00 to 99.
+* Return: 0
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 99; ch++)
+	{
+	putchar((ch / 10) + '0');
+	putchar((ch % 10) + '0');
+	if (ch == 99)
+	{
+		break;
+	}
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
