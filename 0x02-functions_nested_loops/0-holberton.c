d82ba6b85ed084b447dd6d04b77ffc3b684c6bef
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-21 16:08:11 +0000

    Print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main
+* Descroption: Write a program that prints Holberton, followed by a new line.
+* Return: 0
+*/
+int main(void)
+{
+	printf("Holberton");
+	return (0);
+}
