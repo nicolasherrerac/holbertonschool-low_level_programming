9f704bbc2a89803087914354fb397a0c7409272a
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-22 15:27:25 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,7 @@
 */
 int print_last_digit(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 	{
 		_putchar((-n % 10) + '0');
 		return (-n % 10);
@@ -16,5 +16,4 @@ int print_last_digit(int n)
 		_putchar((n % 10) + '0');
 		return (n % 10);
 	}
-	return (0);
 }
