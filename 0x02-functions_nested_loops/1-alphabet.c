6a4037b72d52ddac43a1d76d5328aafc2bdfd3ca
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-22 13:43:24 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
-* main
-* Description: prints the alphabet, in lowercase, followed by a new line.
+* print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+*
 * Return: 0
 */
 void print_alphabet(void)
