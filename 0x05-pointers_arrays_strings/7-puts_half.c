13b28df920bc4acf9cd67b1bc20138d4d7d63951
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-29 22:28:58 +0000

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* puts_half - prints half of a string.
+* @str: pointer
+* Return: 0
+*/
+void puts_half(char *str)
+{
+	int c, c1;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		;
+	}
+	for (c1 = 0; c1 <= (c - 1); c1++)
+	{
+		if (c1 > 4)
+		_putchar(*(str + c1));
+	}
+	_putchar('\n');
+
+}
