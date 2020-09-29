0223387c25af02b5773cd6c3920cfed57e4172c1
nicolasherrerac <nicolasherrerac20@gmail.com>
2020-09-29 02:43:59 +0000

    returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _strlen - returns the length of a string.
+* @s: pointer
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		;
+	}
+	return (a);
+}
