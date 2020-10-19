f940cf9a282ee995456bc21b09334953889676c3
Nicolas <nicolasherrerac20@gmail.com>
2020-10-19 19:24:44 +0000

    correct the if else of the age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,14 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	printf("Age: %f\n", d->age);
+	if (d->age == NULL)
+	{
+		printf("Age: (nill)\n", d->age)
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nill)\n");
