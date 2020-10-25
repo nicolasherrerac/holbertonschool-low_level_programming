0de986b4459356b1238851577463ac6773d045ec
Nicolas <nicolasherrerac20@gmail.com>
2020-10-25 02:11:40 +0000

    missing prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,7 +14,7 @@ typedef struct check
 	void (*f)(va_list);
 } check;
 
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
