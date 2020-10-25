3273a40b758dadf12905d11f517804e8344a58de
Nicolas <nicolasherrerac20@gmail.com>
2020-10-25 03:44:45 +0000

    corrct errors

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,48 @@
 #include <stdarg.h>
 
 /**
+ * print_all - function that prints anything.
+ * @format: list of arguments.
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	check array[] = {
+		{"c", c_char},
+		{"i", c_int},
+		{"f", c_float},
+		{"s", c_string},
+		{NULL, NULL},
+	};
+
+	int i1, i;
+	char *coma = "";
+
+	va_list list;
+
+	va_start(list, format);
+	i =  0;
+	while (format && format[i])
+	{
+		i1 = 0;
+		while (array[i1].check != NULL)
+		{
+			if (format[i] == *array[i1].check)
+			{
+				printf("%s", coma);
+				array[i1].f(list);
+				coma = ", ";
+				break;
+			}
+			i1++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
+
+/**
  * c_char - list of strings.
  * @list: list of arguments.
  * Return: Always 0.
@@ -48,45 +90,4 @@ void c_string(va_list list)
 	{
 		printf("%s", k);
 	}
-}
-/**
-* print_all - function that prints anything.
-* @format: list of arguments.
-* Return: Always 0.
-*/
-void print_all(const char * const format, ...)
-{
-	check array[] = {
-		{"c", c_char},
-		{"i", c_int},
-		{"f", c_float},
-		{"s", c_string},
-		{NULL, NULL},
-	};
-
-	int i1, i;
-	char *coma = "";
-
-	va_list list;
-
-	va_start(list, format);
-	i =  0;
-	while (format && format[i])
-	{
-		i1 = 0;
-		while (array[i1].check != NULL)
-		{
-			if (format[i] == *array[i1].check)
-			{
-				printf("%s", coma);
-				array[i1].f(list);
-				coma = ", ";
-				break;
-			}
-			i1++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(list);
 }
