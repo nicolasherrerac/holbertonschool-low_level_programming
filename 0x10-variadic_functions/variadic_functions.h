45ba3cf9ab677fd8f9b8627e6d741ee96ee0e39a
Nicolas <nicolasherrerac20@gmail.com>
2020-10-25 02:18:16 +0000

    organized prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,21 +3,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void c_char(va_list list);
-void c_int(va_list list);
-void c_float(va_list list);
-void c_string(va_list list);
-
 typedef struct check
 {
 	char *check;
 	void (*f)(va_list);
-} check;
+}check;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void c_char(va_list list);
+void c_int(va_list list);
+void c_float(va_list list);
+void c_string(va_list list);
 
 #endif
