4e448a7141d43e7ae97046282e5afd8f23f5f8a2
Nicolas <nicolasherrerac20@gmail.com>
2020-10-30 17:21:40 +0000

    quit pointer

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,14 @@
 size_t print_list(const list_t *h)
 {
 	int i;
-	const list_t *nodo = h;
 
-	for (i = 0; nodo != NULL; i++)
+	for (i = 0; h != NULL; i++)
 	{
-		if (nodo->str == NULL)
-			printf("[0] (nill)\n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", nodo->len, nodo->str);
-		nodo = nodo->next;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 	}
 	return (i);
 }
