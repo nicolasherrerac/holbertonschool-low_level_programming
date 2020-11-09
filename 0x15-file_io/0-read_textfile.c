cd29fa6736d75e6c40c6cb81a77a3cc132ac153c
Nicolas <nicolasherrerac20@gmail.com>
2020-11-09 17:50:09 +0000

    reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t read_chars, w_error;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
+
+	read_chars = read(fd, buf, letters);
+	if (read_chars < 0)
+		return (0);
+
+	w_error = write(STDOUT_FILENO, buf, read_chars);
+	if (w_error < 0)
+		return (0);
+
+	close(fd);
+	free(buf);
+	return (w_error);
+}
