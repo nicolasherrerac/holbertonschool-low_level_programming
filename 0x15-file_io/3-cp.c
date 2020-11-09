3b2268d2a6fc18463f7791568bc8092432f7fc4a
Nicolas <nicolasherrerac20@gmail.com>
2020-11-09 22:44:51 +0000

    correction 02

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,18 +19,26 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	read_chars = read(file_from, buf, 1024);
+	else
+	{
+		read_chars = read(file_from, buf, 1024);
+		w_error = write(file_to, buf, read_chars);
+	}
+	while (read_chars == 1024);
 	if (read_chars < 0)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	w_error = write(file_to, buf, read_chars);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+		argv[1]), exit(99);
+	if (w_error < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+		argv[2]), exit(99);
 	if (w_error < 0)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (close(file_from) == -1)
@@ -39,4 +47,3 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
-
