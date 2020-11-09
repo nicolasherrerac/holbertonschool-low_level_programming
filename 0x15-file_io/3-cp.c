234b112dd1631e6003677fd8d6ec55e2035173f3
Nicolas <nicolasherrerac20@gmail.com>
2020-11-09 22:51:19 +0000

    correction 03

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,12 +27,11 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	else
-	{
+
+	do {
 		read_chars = read(file_from, buf, 1024);
 		w_error = write(file_to, buf, read_chars);
-	}
-	while (read_chars == 1024);
+	} while (read_chars == 1024);
 	if (read_chars < 0)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 		argv[1]), exit(99);
