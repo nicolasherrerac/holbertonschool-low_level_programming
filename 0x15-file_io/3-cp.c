b4961b44c9cd3bc2eb478cae14de0a7f04b5ddb3
Nicolas <nicolasherrerac20@gmail.com>
2020-11-10 00:32:19 +0000

    correction 08

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-    int file_from, file_to, read_chars, w_error;
+    int file_from, file_to, read_char, w_error;
     char buffer[1024];
 
     if (argc != 3)
@@ -15,25 +15,25 @@ int main(int argc, char *argv[])
     file_to = open(argv[1], O_RDONLY);
     if (file_to < 0)
     {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        dprintf(STDERR_FILENO, "Error: Can't read from file_from %s\n", argv[1]);
         exit(98);
     }
     file_from = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
     if (file_from < 0)
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
     do {
-        read_chars = read(file_to, buffer, 1024);
-        w_error = write(file_from, buffer, len);
-    } while (read_chars == 1024);
-    if (read_chars < 0)
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+        read_char = read(file_to, buffer, 1024);
+        w_error = write(file_from, buffer, read_char);
+    } while (read_char == 1024);
+    if (read_char < 0)
+        dprintf(STDERR_FILENO, "Error: Can't read from file_from %s\n",
         argv[1]), exit(98);
     if (w_error < 0)
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
         argv[2]), exit(99);
-    if (close(w_error) < 0)
+    if (close(file_to) < 0)
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-    if (close(read_chars) < 0)
+    if (close(file_from) < 0)
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
     return (0);
 }
