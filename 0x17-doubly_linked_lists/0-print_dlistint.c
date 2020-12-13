86845c724b7db3e74a0062c3591a3995009cf878
Nicolas <nicolasherrerac20@gmail.com>
2020-12-13 18:53:11 +0000

    Betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * @h: integer.
+ * Return: Number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
