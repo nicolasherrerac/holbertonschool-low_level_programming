f2818caa264a0f33d3fb68d24305c41e167fa99e
Nicolas <nicolasherrerac20@gmail.com>
2020-12-14 15:15:55 +0000

    Fix error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 	}
 	nodo->next = temp->next;
+	nodo->prev = temp;
 	temp->next = nodo;
 	return (nodo);
 }
