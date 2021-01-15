fbe2707d1e3e366fa1d4f300839e96a17e9ee78b
Nicolas <nicolasherrerac20@gmail.com>
2021-01-15 21:14:26 +0000

    Fix error

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,21 +8,22 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node = NULL, *next_node = NULL;
 	unsigned int i;
 
-	if (ht)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		node = ht->array[i];
+		while (node)
 		{
-			node = ht->array[i];
-			while (node)
-			{
-				next_node = node->next;
-				free(node->value);
-				free(node->key);
-				free(node);
-				node = next_node;
-			}
+			next_node = node->next;
+			free(node->value);
+			free(node->key);
+			free(node);
+			node = next_node;
 		}
-		free(ht->array);
-		free(ht);
 	}
+	free(ht->array);
+	free(ht);
 }
+
